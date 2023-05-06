ab0c3ded9a2580195a61d62f9822e7773f01e93b
Amanda Viória <amandavic7576@gmail.com>
2023-05-06 17:33:24 -0300

    deputados

--- componentes/Cabecalho.js
+++ componentes/Cabecalho.js
@@ -5,7 +5,7 @@ const Cabecalho = () => {
   return (
     <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/deputados">Inicio</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
