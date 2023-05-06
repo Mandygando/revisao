748d6dc6c9566cdcf8c9bac93483eae1f5a9212f
Amanda Viória <amandavic7576@gmail.com>
2023-05-06 17:22:32 -0300

    fotos dos deputados e detalhes

---
+++ componentes/Rodape.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const style = {
+  
+    textAlign: 'center',
+    width: '100%',
+  
+    
+}
+
+const Rodape = () => {
+  return (
+    <div  style={{width: '100%'}} className="bg-secondary text-white text-center position-fixed bottom-0">
+      <p style={style}>Rodape</p>
+    </div>
+
+  )
+}
+
+export default Rodape
